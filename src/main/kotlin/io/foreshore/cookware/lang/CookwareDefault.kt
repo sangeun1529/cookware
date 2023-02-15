73335bc1fee329360b2ebd5a4298436cfdc4a172
kkomac <sangeun.kim@rnrvh.com>
2023-02-15 14:03:49 +0900

    feat: added time obj.

---
+++ src/main/kotlin/io/foreshore/cookware/lang/CookwareDefault.kt
@@ -0,0 +1,7 @@
+package io.foreshore.cookware.lang
+
+object CookwareDefault {
+    const val DATE_FORMAT_DEFAULT = "yyyy-MM-dd"
+    const val TIME_FORMAT_DEFAULT = "HH:mm:ss"
+    const val DATE_TIME_FORMAT_DEFAULT = "$DATE_FORMAT_DEFAULT $TIME_FORMAT_DEFAULT"
+}
